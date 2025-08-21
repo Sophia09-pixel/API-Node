cc4240b837e3c5d57fdb7cb0f39d503f4f62ee6a
Sophia de Sousa <sophia.dev0903@gmail.com>
2025-08-20 22:20:52 -0300

    feat: integração com o mongo db e criacao do mongo

---
+++ src/repositories/product-repository.js
@@ -0,0 +1,34 @@
+const Product = require("../models/product"); // importando a model do produto
+
+class ProductRepository {
+  //Buscar todos
+  async findAll() {
+    // buscar todos os produtos ativos {active: true} (filtro)
+    return await Product.find({ active: true });
+  }
+
+  //Busca por id
+  async findById(id) {
+    return await Product.findById({ id });
+  }
+
+  //Criar
+  async create(productData) {
+    //criando uma instancia para persistir os dados do mongo
+    const product = new Product(productData);
+    return await product.save();
+  }
+
+  //Atualizar
+  async update(id, productData) {
+    return await Product.findByIdAndUpdate(id, productData);
+  }
+
+  //delete
+  async delete(id){
+    //passa o id e muda o active para false
+    return await Product.findByIdAndUpdate(id, {active: false})
+  }
+}
+
+module.exports = new ProductRepository();
